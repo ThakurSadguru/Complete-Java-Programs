47bb684d1b00e8f0c0e006abd4865a3103477d0b
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-26 13:02:35 +0530

    Calculator Program

---
+++ Basic Programs/Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the first number");
+    int num1 = scanner.nextInt();
+
+    System.out.println("Enter the second number");
+    int num2 = scanner.nextInt();
+
+    int sum = num1 + num2;
+    System.out.println("Addition : " + sum);
+
+    int substract = num1 - num2;
+    System.out.println("Substraction : " + substract);
+
+    int multiply = num1 * num2;
+    System.out.println("Multiplication : " + multiply);
+
+    int divide = num1 / num2;
+    System.out.println("Divide : " + divide);
+
+    int Remainder = num1 % num2;
+    System.out.println("Remainder : " + Remainder);
+  }
+}
