5f2c28abfada3c8877eaf8df93b7db809ef44a5a
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-26 12:48:41 +0530

    Area of Circle program

---
+++ Basic Programs/CircleArea.java
@@ -0,0 +1,10 @@
+public class CircleArea {
+  public static void main(String[] args) {
+    int r = 12;
+    double pi = 3.14;
+    double area = pi * r * r;
+    double perimeter = 2 * pi * r;
+    System.out.println("Area : " + area);
+    System.out.println("Perimeter : " + perimeter);
+  }
+}
