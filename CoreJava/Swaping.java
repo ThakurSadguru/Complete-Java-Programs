179278c69c7ba0f37275275b653f86efbd003f34
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-27 22:13:21 +0530

    swapping numbers

---
+++ CoreJava/Swaping.java
@@ -0,0 +1,24 @@
+package CoreJava;
+
+public class Swaping {
+
+  public static void main(String[] args) {
+
+    int num1 = 10;
+    int num2 = 20;
+
+    System.out.println("Before Swapping values " + num1 + " " + num2);
+
+    // Logic 1 = Third Varable
+
+    /*
+     * int temp = num1;
+     * num1 = num2;
+     * num2 = temp;
+     */
+
+
+     //Logic 2 
+    System.out.println("After swapping values are ..." + num1 + " " + num2);
+  }
+}
