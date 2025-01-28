fb38a5cb4a26b7ffbe28bbc861625b5bbed6ea77
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-01-28 09:42:36 +0530

    Fibonacci series program

---
+++ CoreJava/Fibonacci.java
@@ -0,0 +1,24 @@
+package CoreJava;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter number of terms");
+    int number = sc.nextInt();
+    int first = 0, second = 1, next;
+
+    System.out.println("Fibonacci series is: ");
+
+    for (int i = 0; i < number; i++) {
+      System.out.println(first);
+      next = second + first;
+      first = second;
+      second = next;
+    }
+
+    sc.close();
+  }
+}
