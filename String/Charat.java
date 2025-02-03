a6455215b96a8bbbe2916ffa9cb03191018d1a7e
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-03 17:39:33 +0530

    Charat index

---
+++ String/Charat.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Charat {
+
+  public static void main(String[] args) {
+    String s2 = "Hello@123";
+
+    System.out.println(s2.charAt(5)); // 1st method
+
+    char ch = s2.charAt(5); // 2nd method
+    System.out.println(ch);
+  }
+}
