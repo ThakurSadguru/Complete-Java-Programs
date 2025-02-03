979be75d266d4aa861ced3a967a73336e7b47fc5
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-03 18:24:17 +0530

    Dictionary Sort

---
+++ String/Dictionarysort.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Arrays;
+
+public class Dictionarysort {
+
+  public static void main(String[] args) {
+    String s1 = "hello welcome to java";
+
+    String a[] = s1.split(" ");
+    for (int i = 0; i < a.length; i++) {
+
+      for (int j = i + 1; j < a.length; j++) {
+        if (a[i].compareTo(a[j]) > 0) {
+          String temp = a[i];
+          a[i] = a[j];
+          a[j] = temp;
+        }
+      }
+    }
+    System.out.println(Arrays.toString(a));
+  }
+}
