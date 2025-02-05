b4780c05980de3d25e8b1087ffd9a709cf592693
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-05 13:35:21 +0530

    palindrome

---
+++ String/Palindrome.java
@@ -0,0 +1,24 @@
+package String;
+
+public class Palindrome {
+
+  public static void main(String[] args) {
+
+    String s1 = "abcddcba";
+    String ans = pal(s1);
+    System.out.println(ans);
+
+    if (s1.equals(ans))
+      System.out.println("same");
+    else
+      System.out.println("not same");
+  }
+
+  private static String pal(String s1) {
+    String b = "";
+    for (int i = s1.length() - 1; i >= 0; i--) {
+      b += s1.charAt(i);
+    }
+    return b;
+  }
+}
