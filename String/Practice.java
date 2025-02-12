c475adba59e4e60d2f71cf9c754abed20d98f771
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-12 12:36:12 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,6 +7,21 @@ public class Practice {
 
   public static void main(String[] args) {
 
-   
+    int a[] = { 5, 2, 4, 8, 1, 3 };
+
+    for (int i = 0; i < a.length; i++) {
+      int min = i;
+
+      for (int j = i + 1; j < a.length; j++) {
+        if (a[j] > a[min])
+          min = j;
+      }
+
+      int temp = a[i];
+      a[i] = a[min];
+      a[min] = temp;
+    }
+
+    System.out.println(Arrays.toString(a));
   }
 }
