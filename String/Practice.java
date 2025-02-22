b3de00db8130491683242c1f9261bab691dfb9f7
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-22 11:54:39 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,6 +7,19 @@ public class Practice {
 
   public static void main(String[] args) {
 
-System.out.println("Hello");
+    int a[] = { 1, 2, 3, 4, 5 };
+    Arrays.sort(a);
+
+    int diff = a[1] - a[0];
+    int cnt = 0;
+
+    for (int i = 0; i < a.length - 1; i++) {
+      if (((a[i + 1]) - (a[i])) == diff)
+        cnt++;
+    }
+    if (cnt == a.length - 1)
+      System.out.println("yes");
+    else
+      System.out.println("No");
   }
 }
