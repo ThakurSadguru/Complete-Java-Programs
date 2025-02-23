15bedabf79c376a9cbdfb830e48bb27572a54acf
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-23 23:13:15 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -8,19 +8,6 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter Array size");
-    int size = sc.nextInt();
-
-    int a[] = new int[size];
-
-    for (int i = 0; i < size; i++) {
-      System.out.println("Enter a number");
-      int num = sc.nextInt();
-      a[i] = num;
-
-    }
-
-    System.out.println(Arrays.toString(a));
+    
   }
 }
