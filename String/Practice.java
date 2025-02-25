6e40a3417be48cf66b25e14cdfd66a501606a191
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-25 21:37:47 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -8,7 +8,21 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello");
+    Scanner sc = new Scanner(System.in);
+    System.err.println("ENTER no");
+    int n = sc.nextInt();
+
+    int i, c = 0, sum = 0;
+
+    for (i = 1; i < n; i++) {
+      if (n % i == 0)
+        sum = sum + i;
+    }
+
+    if (sum == n)
+      System.out.println(n + "is perfect no");
+    else
+      System.out.println(n + "not perfect no");
 
   }
 }
