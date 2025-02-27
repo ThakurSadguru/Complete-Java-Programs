af908a875d34b3c15979a0df5ffada213f9b614c
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-27 18:04:50 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -6,20 +6,16 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a[] = { 55, 33, 22, 11, 44 };
+    char ch[] = { 'h', 'e', 'f', 's' };
 
-    for (int i = 1; i < a.length; i++) {
-      int temp = a[i];
-      int j = i - 1;
-      for (; j >= 0; j--) {
-        if (a[j] > temp)
-          a[j + 1] = a[j];
-        else
-          break;
-      }
-      a[j + 1] = temp;
+    String s1 = "";
+
+    for (int i = 0; i < ch.length; i++) {
+      s1 += ch[i];
     }
-    System.out.println(Arrays.toString(a));
+
+    System.out.println(s1);
+
   }
 
 }
