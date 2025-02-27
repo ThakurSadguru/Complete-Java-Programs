18b9165f6264fe464f1f96124b999d7f1caedb3b
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-27 10:54:14 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -6,35 +6,15 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-    int evencnt = 0;
-    int oddCnt = 0;
+    int a[] = { -1, 2, 3, 1, 5, 4, -8 };
+    int max = 0;
 
     for (int i = 0; i < a.length; i++) {
-      if (a[i] % 2 == 0)
-        evencnt++;
-      else
-        oddCnt++;
+      if (a[i] > max)
+        max = a[i];
     }
 
-    int b[] = new int[evencnt];
-    int c[] = new int[oddCnt];
-
-    int evenIndex = 0;
-    int oddIndex = 0;
-
-    for (int i = 0; i < a.length; i++) {
-      if (a[i] % 2 == 0) {
-        b[evenIndex] = a[i];
-        evenIndex++;
-      } else {
-        c[oddIndex] = a[i];
-        oddIndex++;
-      }
-    }
-
-    System.out.println(Arrays.toString(b));
-    System.out.println(Arrays.toString(c));
+    System.out.println(max);
   }
 
 }
