bccae3baa8f24004d878ba31d79014c07c09e435
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-27 10:04:38 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -4,7 +4,18 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    System.out.println("Hello");
+    int[] a = { 1, 2, 3, 4, 5 };
+    int sum = 0;
+
+    for (int i = 0; i < a.length; i++) {
+      sum = sum + a[i];
+    }
+
+    System.out.println(sum);
+
+    int avg = sum / a.length;
+
+    System.out.println(avg);
   }
 
 }
