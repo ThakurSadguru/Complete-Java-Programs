0c1ed9586d1a453ac03763700866af8743caade8
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-27 10:15:44 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -4,23 +4,27 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int[] a = { 1, 2, 3, 4, 5 };
-    int b[] = { 1, 2, 3, 4, 5 };
+    int a[] = { 1, 2, 3, 4, 5 };
+    int ele = 5;
+    int start = 0;
+    int end = a.length;
+    int cnt = 0;
 
-    if (a.length == b.length) {
-      int cnt = 0;
-      for (int i = 0; i < a.length; i++) {
-        if (a[i] == b[i])
-          cnt++;
+    while (start <= end) {
+      int mid = (start + end) / 2;
+
+      if (a[mid] == ele) {
+        cnt++;
+        System.out.println("found");
+        break;
       }
-      if (cnt == a.length)
-        System.out.println("same");
+      if (a[mid] < ele)
+        start = mid + 1;
       else
-        System.out.println("not same");
-    } else {
-      System.out.println("length");
+        end = mid - 1;
     }
-
+    if (cnt == 0)
+      System.out.println("not found");
   }
 
 }
