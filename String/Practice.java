5780067fc0c80b85f9bfd3a8b4c5bee0d1970ba2
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 10:47:38 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -6,22 +6,23 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a[] = { 5, 3, 4, 2, 9, 8, 5, 1 };
+    int a[] = { 5, 3, 6, 7, 2, 1 };
 
     for (int i = 0; i < a.length; i++) {
       int min = i;
-
       for (int j = i + 1; j < a.length; j++) {
-        if (a[j] > a[min])
+        if (a[j] < a[min]) {
           min = j;
-      }
+        }
 
-      int temp = a[i];
-      a[i] = a[min];
-      a[min] = temp;
+        int temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+      }
     }
 
-    System.out.println(Arrays.toString(a));
+    int diff = a[a.length - 1] - a[0];
+    System.out.println(diff);
   }
 
 }
