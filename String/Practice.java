cc61837e6d13f36ca295ed2a0b70d92193945a4c
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 19:23:48 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,13 +7,21 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    String s1 = "abcd";
-    String s2 = "abfge";
+    String s1 = "hello aadarsh welcome to java";
 
-    String s3 = s1 + "" + s2;
+    String a[] = s1.split(" ");
 
-    System.out.println(s3);
+    for (int i = 0; i < a.length; i++) {
+      for (int j = i + 1; j < a.length; j++) {
+        if (a[i].compareTo(a[j]) > 0) {
+          String temp = a[i];
+          a[i] = a[j];
+          a[j] = temp;
+        }
+      }
+    }
 
+    System.out.println(Arrays.toString(a));
   }
 
 }
