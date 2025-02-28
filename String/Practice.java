e26541a1958efe54cca215d966b0df23cea78c1b
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 19:11:13 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,10 +7,21 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    String s2 = "Sadguru@123";
+    String s1 = "abcd";
+    String s2 = "abfg";
+    int cnt = 0;
 
-    System.out.println(s2.charAt(3));
+    for (int i = 0; i < s1.length() && i < s2.length(); i++) {
+      if (s1.charAt(i) != s2.charAt(i)) {
+        System.out.println(s1.charAt(i) - s2.charAt((i)));
+        cnt++;
+        break;
+      }
 
+    }
+
+    if (cnt == 0)
+      System.out.println(s1.length() - s2.length());
   }
 
 }
