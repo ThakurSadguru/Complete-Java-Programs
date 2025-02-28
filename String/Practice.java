ba413d0a8b8a2f2f1a81dd86660e8f989d7d24a8
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 10:31:32 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -6,30 +6,18 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a[] = { 1, 2, 3, 4, 5, 6 };
-    int ele = 3;
-    int cnt = 0;
-    int start = 0;
-    int end = a.length;
+    int a[] = { 1, 2, 3, 4, 8, 9, 6 };
 
-    while (start <= end) {
-
-      int mid = (start + end) / 2;
-
-      if (a[mid] == ele) {
-        cnt++;
-        System.out.println("Found");
-        break;
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 0; j < a.length - i - 1; i++) {
+        if (a[j] > a[j + 1]) {
+          int temp = a[j];
+          a[j] = a[j + 1];
+          a[j + 1] = temp;
+        }
       }
-
-      if (a[mid] < ele)
-        start = mid + 1;
-      else
-        end = mid - 1;
     }
-
-    if (cnt == 0)
-      System.out.println("not found");
+    System.out.println(Arrays.toString(a));
   }
 
 }
