ccee3a54d6e282a737a69428f5ba061f73c09985
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 10:19:46 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -6,20 +6,23 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a[] = { 1, 2, 3, 4, 5 };
+    int a[] = { 1, 2, 3, 4, };
 
-    int sum = 0;
+    int b[] = { 1, 2, 3, 4, 9 };
 
-    for (int i = 0; i < a.length; i++) {
-      sum += a[i];
-    }
+    if (a.length == b.length) {
+      int cnt = 0;
+      for (int i = 0; i < a.length; i++) {
+        if (a[i] == b[i])
+          cnt++;
+      }
+      if (cnt == a.length)
+        System.out.println("same");
+      else
+        System.out.println(" not Same");
 
-    System.out.println(sum);
-
-    int avg = sum / a.length;
-
-    System.out.println(avg);
-
+    } else
+      System.out.println("length");
   }
 
 }
