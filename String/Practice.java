f34531f5c8118f86ba2e28fc213f3ca298847376
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 10:36:07 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -6,18 +6,24 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a[] = { 1, 2, 3, 4, 8, 9, 6 };
+    int a[] = { 5, 3, 4, 2, 1 };
 
-    for (int i = 0; i < a.length; i++) {
-      for (int j = 0; j < a.length - i - 1; i++) {
-        if (a[j] > a[j + 1]) {
-          int temp = a[j];
-          a[j] = a[j + 1];
-          a[j + 1] = temp;
-        }
+    Arrays.sort(a);
+
+    int diff = a[1] - a[0];
+
+    int cnt = 0;
+
+    for (int i = 0; i < a.length - 1; i++) {
+      if (((a[i + 1]) - (a[i])) == diff) {
+        cnt++;
       }
     }
-    System.out.println(Arrays.toString(a));
+
+    if (cnt == a.length - 1)
+      System.out.println("same");
+    else
+      System.out.println("no");
   }
 
 }
