9b941e95bd4d2adce9a7bd538b57eb4cd6115265
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 19:16:12 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -8,20 +8,23 @@ public class Practice {
   public static void main(String[] args) {
 
     String s1 = "abcd";
-    String s2 = "abfg";
+    String s2 = "abfge";
     int cnt = 0;
 
-    for (int i = 0; i < s1.length() && i < s2.length(); i++) {
-      if (s1.charAt(i) != s2.charAt(i)) {
-        System.out.println(s1.charAt(i) - s2.charAt((i)));
-        cnt++;
-        break;
+    if (s1.length() == s2.length()) {
+      for (int i = 0; i < s1.length(); i++) {
+        if (s1.charAt(i) == s2.charAt(i)) {
+          cnt++;
+        }
+
       }
-
-    }
-
-    if (cnt == 0)
-      System.out.println(s1.length() - s2.length());
+      if (cnt == s1.length()) {
+        System.out.println("Same");
+      } else {
+        System.out.println("not same");
+      }
+    } else
+      System.out.println("Length");
   }
 
 }
