be299c969cf81636ab94a139b99d1ee19332adad
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 19:02:41 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,25 +7,13 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int num = 81;
-    int temp = num;
-    System.out.println("Number is : " + num);
-
-    int sum = 0;
-    while (num > 0) {
-
-      int rem = num % 10;
-      sum = sum + rem;
-      num = num / 10;
+    char ch[] = { 'h', 'e', 'f', 's' };
+    String s1 = "";
+    for (int i = 0; i < ch.length; i++) {
+      s1 += ch[i];
     }
 
-    System.out.println("Sum " + sum);
-
-    if (temp % sum == 0) {
-      System.out.println("harshad");
-    } else {
-      System.out.println("no");
-    }
+    System.out.println(s1);
   }
 
 }
