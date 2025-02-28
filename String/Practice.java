b8e0bf8d6053d5a9157f7bcc1c2b66f417f28ac1
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-02-28 10:21:57 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -6,23 +6,13 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int a[] = { 1, 2, 3, 4, };
+    int ar[] = new int[3];
 
-    int b[] = { 1, 2, 3, 4, 9 };
+    ar[0] = 1;
+    ar[1] = 4;
+    ar[2] = 3;
 
-    if (a.length == b.length) {
-      int cnt = 0;
-      for (int i = 0; i < a.length; i++) {
-        if (a[i] == b[i])
-          cnt++;
-      }
-      if (cnt == a.length)
-        System.out.println("same");
-      else
-        System.out.println(" not Same");
-
-    } else
-      System.out.println("length");
+    System.out.println(Arrays.toString(ar));
   }
 
 }
