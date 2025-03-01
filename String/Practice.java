2c5f41b52b0d6d9416c201f413849de49199d07b
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-03-01 10:10:15 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,32 +7,18 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    String s1 = "sadguru";
-    String s2 = "urulllppppp";
+    int a[] = { 1, 2, 3, 4, 5 };
 
-    if (s1.length() >= s2.length()) {
-      int s1Index = s1.length() - 1;
-      int s2Index = s2.length() - 1;
-      int cnt = 0;
+    int sum = 0;
 
-      while (s2Index >= 0) {
-        if (s2.charAt(s2Index) == s1.charAt(s1Index)) {
-          cnt++;
-        }
-
-        s1Index--;
-        s2Index--;
-      }
-
-      if (cnt == s2.length())
-        System.out.println("same");
-      else
-        System.out.println("not same");
+    for (int i = 0; i < a.length; i++) {
+      sum += a[i];
     }
 
-    else {
-      System.out.println("length ");
-    }
+    int avg = sum / a.length;
+
+    System.out.println(avg);
+
   }
 
 }
