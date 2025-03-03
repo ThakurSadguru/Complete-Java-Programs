8d0a060261744aef583c1ea71bc3e474bf77fc9c
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-03-03 21:13:36 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,28 +7,7 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int num = 9;
-    int temp = num;
-
-    int sq = num * num;
-    int cnt = 0;
-
-    while (num > 0) {
-      num = num / 10;
-      cnt++;
-    }
-
-    int div = (int) Math.pow(10, cnt);
-
-    int fpart = sq / div;
-    int spart = sq % div;
-
-    int add = fpart + spart;
-
-    if (add == temp)
-      System.out.println("K");
-    else
-      System.out.println("N");
-
+    
+    
   }
 }
