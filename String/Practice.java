9a8d8eb61b82d04fa9caec901659fa1b77256b62
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-03-06 19:25:18 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,7 +7,30 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    
-    
+    String s1 = "sadguru";
+    String s2 = "uru";
+
+    if (s1.length() >= s2.length()) {
+      int s1Index = s1.length() - 1;
+      int s2Index = s2.length() - 1;
+      int cnt = 0;
+
+      while (s2Index >= 0) {
+        if (s2.charAt(s2Index) == s1.charAt(s1Index)) {
+          cnt++;
+        }
+        s1Index--;
+        s2Index--;
+      }
+
+      if (cnt == s2.length())
+        System.out.println("same");
+      else
+        System.out.println("not same");
+    }
+
+    else
+      System.out.println("not");
+
   }
 }
