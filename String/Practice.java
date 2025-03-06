6d94cb870c11fe0bf0104067d4fb3433ac69cff1
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-03-06 19:45:22 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,39 +7,24 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    String s1 = "sadguru";
+    String s1 = "abcba";
+    String ans = pal(s1);
 
-    freq(s1);
+    System.out.println(ans);
+
+    if (s1.equals(ans))
+      System.out.println("same");
+    else
+      System.out.println("not same");
 
   }
 
-  private static void freq(String s1) {
-    int max = 0;
-    char ch = 0;
-
-    for (int i = 0; i < s1.length(); i++) {
-      int cnt = 0;
-      for (int j = 0; j < i; j++) {
-        if (s1.charAt(i) == s1.charAt(j)) {
-          cnt++;
-        }
-      }
-      if (cnt == 0) {
-        int freq = 0;
-        for (int j = 0; j < s1.length(); j++) {
-          if (s1.charAt(i) == s1.charAt(j)) {
-            freq++;
-          }
-
-        }
-
-        if (freq > max) {
-          max = freq;
-          ch = s1.charAt(i);
-        }
-      }
+  private static String pal(String s1) {
+    String b = "";
+    for (int i = s1.length() - 1; i >= 0; i--) {
+      b += s1.charAt(i);
     }
 
-    System.out.println(ch + " " + max);
+    return b;
   }
 }
