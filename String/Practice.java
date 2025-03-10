c92a7e73a48d23b54ceeeeda31cd7359376ab6b0
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-03-10 19:32:36 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,22 +7,7 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    int space = 4;
-
-    for (int i = 1; i <= 5; i++) {
-      int cnt = 5;
-      for (int j = 1; j <= 5; j++) {
-        if (j <= space) {
-          System.out.print(" ");
-        } else {
-          System.out.print(cnt);
-          cnt--;
-        }
-      }
-      System.out.println();
-      space--;
-
-    }
+    
 
   }
 }
