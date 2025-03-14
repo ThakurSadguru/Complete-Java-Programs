d3831dcf9b037336f1193cb37a7049990f703d13
ThakurSadguru <sadgurusinghthakur@gmail.com>
2025-03-14 17:21:20 +0530

    practice

--- String/Practice.java
+++ String/Practice.java
@@ -7,6 +7,25 @@ public class Practice {
 
   public static void main(String[] args) {
 
-    System.out.println("hello");
+    int end = 5;
+    int space = 0;
+
+    for (int i = 0; i <= 9; i++) {
+      for (int j = 0; j <= space; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j <= end; j++) {
+        System.out.print("* ");
+      }
+
+      System.out.println();
+      if (i < 5) {
+        end--;
+        space++;
+      } else {
+        end++;
+        space--;
+      }
+    }
   }
 }
